fae2db836e7570bd4532216459d0b159f3c09199
amisha237 <47049886+amisha237@users.noreply.github.com>
2019-09-22 13:39:43 +0530

    Login Signup updated

--- components/Login.vue
+++ components/Login.vue
@@ -1,91 +1,65 @@
-  <template>
-     <v-dialog v-model="dialog"  max-width="700px" >
-        <template v-slot:activator="{ on }">
-         <v-btn color="primary" text v-on="on">Login | Sign Up</v-btn>
-        </template>
-        <template>
-           <v-tabs
-              v-model="active"
-              color="primary"
-              light
-              slider-color="yellow"
-              grow
-           >
-             <v-tab
-              v-for="n in logins"
-              :key="n"
-              ripple
-              >
-              {{n}}
-
-              </v-tab>
-                 <v-tab-item >
-                   <v-card>
-                    <v-card-text>
-                     <div>
-	                    <v-form @submit.prevent="checkLogin" id="login-form"  method="post">
-                        <v-text-field
-                        prepend-icon="person"
-                        label="Email ID"
-                        placeholder=" "
-                        name="email"
-                        type="email"
-                        v-model="email"
-                        autocomplete="off"
-                        required
-                        outline
-                        hide-details
-                        >
-                       </v-text-field>
-                        <v-text-field id="password"
-                          prepend-icon="lock"
-                          v-model="password"
-                          :append-icon="show ? 'visibility' : 'visibility_off'"
-                          :type="show ? 'text' : 'password'"
-                          name="password"
-                          label="Password"
-                          placeholder=" "
-                          hint="At least 8 characters"
-                          autocomplete="off"
-                          @click:append="show = !show"
-                          outline
-                          >
-                        </v-text-field>
-
-                         <v-checkbox
-                           v-model="checkbox"
-                           label="Remember Me"
-                         ></v-checkbox>
-                         <v-flex xs12 sm12 md12 lg12>
-                        <center>
-                         <v-btn color="success" type="submit" left>LogIn</v-btn>
-                        </center>
-                         </v-flex>
-                     </v-form>
-                    </div>
-                     </v-card-text>
-                    </v-card>
-                  </v-tab-item>
-                  <v-tab-item>
+<template>
+   <v-card>
+       <v-card-text>
+          <div>
+              <v-form @submit.prevent="checkLogin" id="login-form"  method="post">
+              
+                <!-- email -->
+                 <v-text-field
+                    prepend-icon="person"
+                      label="Email ID"                                    
+                      placeholder=" "                                   
+                      name="email"
+                      type="email"
+                      v-model="email"
+                      autocomplete="off"
+                      required
+                      outline
+                      hide-details
+                      >                                      
+                  </v-text-field>
                     
-                    <signup />
-                  </v-tab-item>
-              </v-tabs>
-           </template>
-      </v-dialog>
+                    <!-- password -->
+                  <v-text-field id="password"
+                      prepend-icon="lock"
+                      v-model="password"
+                      :append-icon="show ? 'visibility' : 'visibility_off'"
+                      :type="show ? 'text' : 'password'"
+                      name="password"
+                      label="Password"
+                      placeholder=" "
+                      hint="At least 8 characters"
+                      autocomplete="off"
+                      @click:append="show = !show"
+                      outline
+                       >
+                  </v-text-field>
+
+                <!-- remember me -->
+                  <v-checkbox
+                      v-model="checkbox"
+                      label="Remember Me"
+                  >
+                  </v-checkbox>
+
+                <!-- submit button -->
+                  <v-flex xs12 sm12 md12 lg12>
+                      <center>
+                      <v-btn color="success" type="submit" left>LogIn</v-btn>
+                      </center>
+                      </v-flex>
+              </v-form>
+          </div>
+      </v-card-text>
+    </v-card>
 </template>
+  
 
 <script>
-import signup from './signup';
-
 export default {
-  components:{
-    signup
-  },
+
     data() {
 	  	return {
-        active:null,
-		  	dialog:false,
         email: '',
 		   	password: '',
 		  	show: false,
@@ -95,7 +69,6 @@ export default {
 				emailValid : v=> /.+@.+/.test(v) || 'E-mail must be valid'
 			},
         checkbox: false,
-        logins:[ 'Sign In', 'Sign Up' ]
 
 		}
 	}
