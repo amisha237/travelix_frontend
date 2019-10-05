131aee84e2355b4c8a363463e2dc7e3aa8e328a2
saumyagoyal1708 <1705071@kiit.ac.in>
2019-10-05 22:40:00 +0530

    add blog page complete

--- pages/admin/add-blog.vue
+++ pages/admin/add-blog.vue
@@ -11,15 +11,85 @@
                     method="POST"
                     id="form">
                         <v-row>
-                            <v-col cols = "12" xs = "6" sm = "6" md = "6" lg = "6">
-                                <p class = "black--text">Select City :</p>
+                            <v-col cols = "12" xs = "12" sm = "12" md = "12" lg = "12">
+                                <v-select
+                                :items="items"
+                                label="Select City"
+                                outlined
+                            ></v-select>
                             </v-col>
-                            <v-col cols = "12" xs = "6" sm = "6" md = "6" lg = "6">
-                                
+
+                        </v-row>
+                        <v-row>
+                            <v-col cols = "12" xs = "12" sm = "12" md = "12" lg = "12">
+                                <v-text-field
+                                label="Add Place name"
+                                outlined
+                                ></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols = "12" xs = "12" sm = "12" md = "12" lg = "12">
+                                <v-file-input 
+                                label="Add Image" 
+                                outlined dense
+                                :rules="rules"
+                                prepend-icon="mdi-camera"
+                                accept="image/png, image/jpeg, image/bmp">
+
+                                </v-file-input>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols = "12" xs = "5" sm = "5" md = "5" lg = "5">
+                            </v-col>
+                            <v-col cols = "12" xs = "2" sm = "2" md = "2" lg = "2">
+                                <v-btn
+                                color = "primary"> 
+                                Submit
+                                </v-btn>
                             </v-col>
                         </v-row>
 
                      </v-form>
+             </v-card>
+        </v-col>
+    </v-row>
+
+    <v-row>
+        <v-col cols = "12" xs = "12" sm = "12" md = "12" lg = "12">
+             <v-card class="mx-auto pa-10 ma-10 grey lighten-3" >
+          
+                    <v-card-text class="black--text display-1 text-center" text-center>Add City</v-card-text>
+                     <v-form
+                    ref="form1"
+                    v-model="active"
+                    method="POST"
+                    id="form1">
+                        <v-row>
+                           <v-col cols = "12" xs = "12" sm = "12" md = "12" lg = "12">
+                               <v-select
+                                :items="cities"
+                                label="Select City"
+                                outlined
+                            ></v-select>
+                           </v-col> 
+                        </v-row>
+                        <v-row>
+                            <v-col cols = "12" xs = "12" sm = "12" md = "12" lg = "12">
+                                <v-file-input 
+                                label="Add Header Image" 
+                                outlined dense
+                                :rules="rules"
+                                prepend-icon="mdi-camera"
+                                accept="image/png, image/jpeg, image/bmp">
+
+                                </v-file-input>
+                            </v-col>
+                        </v-row>
+
+                     </v-form>
+             </v-card>
         </v-col>
     </v-row>
 </v-container>
@@ -28,6 +98,15 @@
 
 <script>
 export default {
+    data(){
+        return{
+            items:['Delhi' , 'Mumbai','Chennai','Kolkata'],
+            cities :['Jaipur','Andaman & Nicobar' ,'Nainital','Kulu Manali'],
+            rules: [
+                value => !value || value.size < 2000000 || 'Avatar size should be less than 2 MB!',
+                ],
+        }
+    }
 
 }
 </script>
