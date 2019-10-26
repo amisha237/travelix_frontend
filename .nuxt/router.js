b21dd8693f656de15a09417b6a14a1c238d1eb5a
saumyagoyal1708 <1705071@kiit.ac.in>
2019-10-26 14:05:30 +0530

    add stories page

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,28 +3,29 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _172e437c = () => interopDefault(import('..\\pages\\admin\\index.vue' /* webpackChunkName: "pages_admin_index" */))
-const _de4fad0a = () => interopDefault(import('..\\pages\\blog.vue' /* webpackChunkName: "pages_blog" */))
-const _403b6ad7 = () => interopDefault(import('..\\pages\\contact.vue' /* webpackChunkName: "pages_contact" */))
-const _61d266de = () => interopDefault(import('..\\pages\\inspire.vue' /* webpackChunkName: "pages_inspire" */))
-const _73f8f7fd = () => interopDefault(import('..\\pages\\package.vue' /* webpackChunkName: "pages_package" */))
-const _43cfd5f2 = () => interopDefault(import('..\\pages\\package-details.vue' /* webpackChunkName: "pages_package-details" */))
-const _0ed1a8c6 = () => interopDefault(import('..\\pages\\packages.vue' /* webpackChunkName: "pages_packages" */))
-const _79f554ec = () => interopDefault(import('..\\pages\\stories.vue' /* webpackChunkName: "pages_stories" */))
-const _0528f766 = () => interopDefault(import('..\\pages\\admin\\add-admin.vue' /* webpackChunkName: "pages_admin_add-admin" */))
-const _2e3642d8 = () => interopDefault(import('..\\pages\\admin\\add-blog.vue' /* webpackChunkName: "pages_admin_add-blog" */))
-const _88cd8846 = () => interopDefault(import('..\\pages\\admin\\add-city.vue' /* webpackChunkName: "pages_admin_add-city" */))
-const _3a80ff78 = () => interopDefault(import('..\\pages\\admin\\add-package.vue' /* webpackChunkName: "pages_admin_add-package" */))
-const _57287b3e = () => interopDefault(import('..\\pages\\admin\\dashboard.vue' /* webpackChunkName: "pages_admin_dashboard" */))
-const _1145d7a2 = () => interopDefault(import('..\\pages\\admin\\view-blogs.vue' /* webpackChunkName: "pages_admin_view-blogs" */))
-const _1966960c = () => interopDefault(import('..\\pages\\admin\\view-bookings.vue' /* webpackChunkName: "pages_admin_view-bookings" */))
-const _77b6d31a = () => interopDefault(import('..\\pages\\admin\\view-customers.vue' /* webpackChunkName: "pages_admin_view-customers" */))
-const _fdf74682 = () => interopDefault(import('..\\pages\\admin\\view-enquiries.vue' /* webpackChunkName: "pages_admin_view-enquiries" */))
-const _386bd63f = () => interopDefault(import('..\\pages\\admin\\view-packages.vue' /* webpackChunkName: "pages_admin_view-packages" */))
-const _45b9d61e = () => interopDefault(import('..\\pages\\admin\\view-stories.vue' /* webpackChunkName: "pages_admin_view-stories" */))
-const _398826a9 = () => interopDefault(import('..\\pages\\admin\\view-subscribers.vue' /* webpackChunkName: "pages_admin_view-subscribers" */))
-const _5d3eecb4 = () => interopDefault(import('..\\pages\\admin\\view-users.vue' /* webpackChunkName: "pages_admin_view-users" */))
-const _22724249 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
+const _02abb03c = () => interopDefault(import('..\\pages\\admin\\index.vue' /* webpackChunkName: "pages_admin_index" */))
+const _0459d3e1 = () => interopDefault(import('..\\pages\\blog.vue' /* webpackChunkName: "pages_blog" */))
+const _42e0429e = () => interopDefault(import('..\\pages\\contact.vue' /* webpackChunkName: "pages_contact" */))
+const _6d6b406b = () => interopDefault(import('..\\pages\\inspire.vue' /* webpackChunkName: "pages_inspire" */))
+const _124d6bd7 = () => interopDefault(import('..\\pages\\package.vue' /* webpackChunkName: "pages_package" */))
+const _4e47cfcc = () => interopDefault(import('..\\pages\\package-details.vue' /* webpackChunkName: "pages_package-details" */))
+const _3b0bb02c = () => interopDefault(import('..\\pages\\packages.vue' /* webpackChunkName: "pages_packages" */))
+const _6159c964 = () => interopDefault(import('..\\pages\\stories.vue' /* webpackChunkName: "pages_stories" */))
+const _41f2c5b3 = () => interopDefault(import('..\\pages\\admin\\add-admin.vue' /* webpackChunkName: "pages_admin_add-admin" */))
+const _19464f24 = () => interopDefault(import('..\\pages\\admin\\add-blog.vue' /* webpackChunkName: "pages_admin_add-blog" */))
+const _73dd9492 = () => interopDefault(import('..\\pages\\admin\\add-city.vue' /* webpackChunkName: "pages_admin_add-city" */))
+const _227c002a = () => interopDefault(import('..\\pages\\admin\\add-package.vue' /* webpackChunkName: "pages_admin_add-package" */))
+const _71885db7 = () => interopDefault(import('..\\pages\\admin\\add-stories.vue' /* webpackChunkName: "pages_admin_add-stories" */))
+const _c8a086b8 = () => interopDefault(import('..\\pages\\admin\\dashboard.vue' /* webpackChunkName: "pages_admin_dashboard" */))
+const _43bdff89 = () => interopDefault(import('..\\pages\\admin\\view-blogs.vue' /* webpackChunkName: "pages_admin_view-blogs" */))
+const _5c02b472 = () => interopDefault(import('..\\pages\\admin\\view-bookings.vue' /* webpackChunkName: "pages_admin_view-bookings" */))
+const _55e77666 = () => interopDefault(import('..\\pages\\admin\\view-customers.vue' /* webpackChunkName: "pages_admin_view-customers" */))
+const _dc27e9ce = () => interopDefault(import('..\\pages\\admin\\view-enquiries.vue' /* webpackChunkName: "pages_admin_view-enquiries" */))
+const _7b07f4a5 = () => interopDefault(import('..\\pages\\admin\\view-packages.vue' /* webpackChunkName: "pages_admin_view-packages" */))
+const _d612dc6a = () => interopDefault(import('..\\pages\\admin\\view-stories.vue' /* webpackChunkName: "pages_admin_view-stories" */))
+const _a184b2fa = () => interopDefault(import('..\\pages\\admin\\view-subscribers.vue' /* webpackChunkName: "pages_admin_view-subscribers" */))
+const _1dc17500 = () => interopDefault(import('..\\pages\\admin\\view-users.vue' /* webpackChunkName: "pages_admin_view-users" */))
+const _1f25e4a3 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
 
 Vue.use(Router)
 
@@ -37,91 +38,95 @@ export const routerOptions = {
 
   routes: [{
       path: "/admin",
-      component: _172e437c,
+      component: _02abb03c,
       name: "admin"
     }, {
       path: "/blog",
-      component: _de4fad0a,
+      component: _0459d3e1,
       name: "blog"
     }, {
       path: "/contact",
-      component: _403b6ad7,
+      component: _42e0429e,
       name: "contact"
     }, {
       path: "/inspire",
-      component: _61d266de,
+      component: _6d6b406b,
       name: "inspire"
     }, {
       path: "/package",
-      component: _73f8f7fd,
+      component: _124d6bd7,
       name: "package"
     }, {
       path: "/package-details",
-      component: _43cfd5f2,
+      component: _4e47cfcc,
       name: "package-details"
     }, {
       path: "/packages",
-      component: _0ed1a8c6,
+      component: _3b0bb02c,
       name: "packages"
     }, {
       path: "/stories",
-      component: _79f554ec,
+      component: _6159c964,
       name: "stories"
     }, {
       path: "/admin/add-admin",
-      component: _0528f766,
+      component: _41f2c5b3,
       name: "admin-add-admin"
     }, {
       path: "/admin/add-blog",
-      component: _2e3642d8,
+      component: _19464f24,
       name: "admin-add-blog"
     }, {
       path: "/admin/add-city",
-      component: _88cd8846,
+      component: _73dd9492,
       name: "admin-add-city"
     }, {
       path: "/admin/add-package",
-      component: _3a80ff78,
+      component: _227c002a,
       name: "admin-add-package"
     }, {
+      path: "/admin/add-stories",
+      component: _71885db7,
+      name: "admin-add-stories"
+    }, {
       path: "/admin/dashboard",
-      component: _57287b3e,
+      component: _c8a086b8,
       name: "admin-dashboard"
     }, {
       path: "/admin/view-blogs",
-      component: _1145d7a2,
+      component: _43bdff89,
       name: "admin-view-blogs"
     }, {
       path: "/admin/view-bookings",
-      component: _1966960c,
+      component: _5c02b472,
       name: "admin-view-bookings"
     }, {
       path: "/admin/view-customers",
-      component: _77b6d31a,
+      component: _55e77666,
       name: "admin-view-customers"
     }, {
       path: "/admin/view-enquiries",
-      component: _fdf74682,
+      component: _dc27e9ce,
       name: "admin-view-enquiries"
     }, {
       path: "/admin/view-packages",
-      component: _386bd63f,
+      component: _7b07f4a5,
       name: "admin-view-packages"
     }, {
       path: "/admin/view-stories",
-      component: _45b9d61e,
+      component: _d612dc6a,
       name: "admin-view-stories"
     }, {
       path: "/admin/view-subscribers",
-      component: _398826a9,
+      component: _a184b2fa,
       name: "admin-view-subscribers"
     }, {
       path: "/admin/view-users",
-      component: _5d3eecb4,
+      component: _1dc17500,
       name: "admin-view-users"
     }, {
       path: "/",
-      component: _22724249,
+      component: _1f25e4a3,
       name: "index"
     }],
 
---
+++ pages/admin/add-stories.vue
@@ -0,0 +1,135 @@
+<template>
+<v-container fluid>
+     <v-row>
+        <v-col cols = "12" xs = "12" sm = "12" md = "12" lg = "12">
+             <v-card class="mx-auto pa-10 ma-10 grey lighten-3" >
+          
+                    <v-card-text class="black--text display-1 text-center" text-center>Add Stories</v-card-text>
+                     <v-form
+                    ref="form"
+                    v-model="active"
+                    method="POST"
+                    id="form">
+                        <v-row>
+                            <v-col cols = "12" xs = "12" sm = "12" md = "12" lg = "12">
+                                <v-select
+                                v-model="city"
+                                :items="items"
+                                label="Select City"
+                                outlined
+                            ></v-select>
+                            </v-col>
+
+                        </v-row>
+                        <v-row>
+                            <v-col cols = "12" xs = "12" sm = "12" md = "12" lg = "12">
+                                <v-text-field
+                                v-model="place"
+                                label="Add Place name"
+                                outlined
+                                ></v-text-field>
+                            </v-col>
+                        </v-row>
+                         <v-textarea
+                        v-model="description"
+                        label="Details"
+                        required
+                        outlined
+                        >
+                        </v-textarea>
+                        <v-row>
+                            <v-col cols = "6" xs = "6" sm = "6" md = "6" lg = "6">
+                                <v-file-input 
+                                label="Add Image" 
+                                outlined dense
+                                :rules="rules"
+                                prepend-icon="mdi-camera"
+                                accept="image/*"
+                                @change="onFileChanged">
+                                </v-file-input>
+                            </v-col>
+                             <v-col cols = "6" xs = "6" sm = "6" md = "6" lg = "6">
+                                <v-file-input 
+                                label="Add Image" 
+                                outlined dense
+                                :rules="rules"
+                                prepend-icon="mdi-camera"
+                                accept="image/*"
+                                @change="onFileChanged">
+                                </v-file-input>
+                            </v-col>
+                             <v-col cols = "6" xs = "6" sm = "6" md = "6" lg = "6">
+                                <v-file-input 
+                                label="Add Image" 
+                                outlined dense
+                                :rules="rules"
+                                prepend-icon="mdi-camera"
+                                accept="image/*"
+                                @change="onFileChanged">
+                                </v-file-input>
+                            </v-col>
+                             <v-col cols = "6" xs = "6" sm = "6" md = "6" lg = "6">
+                                <v-file-input 
+                                label="Add Image" 
+                                outlined dense
+                                :rules="rules"
+                                prepend-icon="mdi-camera"
+                                accept="image/*"
+                                @change="onFileChanged">
+                                </v-file-input>
+                            </v-col>
+                             <v-col cols = "6" xs = "6" sm = "6" md = "6" lg = "6">
+                                <v-file-input 
+                                label="Add Image" 
+                                outlined dense
+                                :rules="rules"
+                                prepend-icon="mdi-camera"
+                                accept="image/*"
+                                @change="onFileChanged">
+                                </v-file-input>
+                            </v-col>
+                        </v-row>
+                       
+                        <v-row>
+                            <v-col cols = "12" xs = "5" sm = "5" md = "5" lg = "5">
+                            </v-col>
+                            <v-col cols = "12" xs = "2" sm = "2" md = "2" lg = "2">
+                                <v-btn
+                                color = "primary"
+                                @click="submitForm"> 
+                                Submit
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+
+                     </v-form>
+             </v-card>
+        </v-col>
+    </v-row>
+</v-container>
+    
+</template>
+
+<script>
+export default {
+     layout:"adminDashboardNavigation",
+    data(){
+        return{
+            city:'',
+            place:'',
+            description:'',
+            image:'',
+            items:['a','b'],
+            rules: [
+                value => !value || value.size < 2000000 || 'Image size should be less than 2 MB!',
+                ],
+        }
+    },
+    
+
+}
+</script>
+
+<style>
+
+</style>
