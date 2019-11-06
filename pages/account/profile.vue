b652fc9a6c651823f565a320ccf370ddc2df6b77
saumyagoyal1708 <1705071@kiit.ac.in>
2019-11-07 00:04:26 +0530

    profile image upload form

--- pages/account/profile.vue
+++ pages/account/profile.vue
@@ -19,6 +19,76 @@
                         {{item.value}}
                     </v-col>
                 </v-row>
+                <br><br><br>
+                <v-row>
+                    
+                    
+                        <div class="text-center">
+                            <v-dialog
+                                v-model="imagedialog"
+                                width="500"
+                            >
+                                <template v-slot:activator="{ on }">
+                                <v-btn
+                                    color="red lighten-2"
+                                    dark
+                                    v-on="on"
+                                >
+                                    Upload image
+                                </v-btn>
+                                </template>
+                                <v-card>
+                                     <v-form
+                                        ref=""
+                                        v-model="active"
+                                        method="POST"
+                                        enctype="multipart/form-data"
+                                        id="">
+                                        <v-file-input
+                                            
+                                            placeholder="Pick an profile image"
+                                            prepend-icon="mdi-camera"
+                                            show-size
+                                            accept="image/*"
+                                            
+                                            ></v-file-input>
+                                            
+                                            <v-btn color="blue darken-1" text @click="imagedialog = false">Close</v-btn>
+                                            <v-btn color="blue darken-1" text >Save</v-btn>
+                                        
+                                        </v-form>
+                                </v-card>
+                               <!-- <v-card>
+                                <v-card-title
+                                    class="headline grey lighten-2"
+                                    primary-title
+                                >
+                                    Privacy Policy
+                                </v-card-title>
+                        
+                                <v-card-text>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </v-card-text>
+                        
+                                <v-divider></v-divider>
+                        
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn
+                                    color="primary"
+                                    text
+                                    @click="dialog = false"
+                                    >
+                                    I accept
+                                    </v-btn>
+                                </v-card-actions>
+                                </v-card>-->
+                            </v-dialog>
+                            </div>
+                    
+
+
+                </v-row>
             </v-card-content>
         </v-card>
     </div>
@@ -33,6 +103,7 @@ export default {
         // lastName: '',
         // username: '',
         // contact: '',
+        imagedialog: false,
         profileItems: [
             {
                 title: 'Email Id',
